a53ffc351d9aa3cadbcb43e54183494ebd904fed
Arif Islam <prob_sol56@hotmail.com>
2026-04-14 06:28:47 +0600

    feat: initialize API program entry point with dependency injection and middleware configuration

--- LibraryManagementApp.API/Program.cs
+++ LibraryManagementApp.API/Program.cs
@@ -1,15 +1,18 @@
 using LibraryManagementApp.Application;
 using LibraryManagementApp.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-builder.Services.AddProblemDetails();
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
-
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+
 var app = builder.Build();
 
 app.UseExceptionHandler();
@@ -21,7 +24,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
