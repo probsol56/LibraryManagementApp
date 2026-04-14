c7adaab1440541eebd63d4d8cc8d792c5b5962d0
Arif Islam <prob_sol56@hotmail.com>
2026-04-14 06:06:52 +0600

    feat: implement dependency injection configuration for Application and Infrastructure layers

--- LibraryManagementApp.Application/DependencyInjection.cs
+++ LibraryManagementApp.Application/DependencyInjection.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using LibraryManagementApp.Application.Common.Behaviors;
 
 namespace LibraryManagementApp.Application;
 
