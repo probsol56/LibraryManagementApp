234f30ab19f6f8a3c1b41919b140734745e759e6
Arif Islam <prob_sol56@hotmail.com>
2026-04-14 06:24:27 +0600

    feat: configure API pipeline and add validation rules for CreateBook command

--- LibraryManagementApp.API/Program.cs
+++ LibraryManagementApp.API/Program.cs
@@ -1,7 +1,8 @@
 using LibraryManagementApp.Application;
 using LibraryManagementApp.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -10,6 +11,8 @@ builder.Services.AddControllers();
 
 builder.Services.AddApplication();
 var app = builder.Build();
+
+app.UseExceptionHandler();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- LibraryManagementApp.Application/Books/Commands/CreateBook/CreateBookCommandValidator.cs
+++ LibraryManagementApp.Application/Books/Commands/CreateBook/CreateBookCommandValidator.cs
@@ -9,7 +9,7 @@ public class CreateBookCommandValidator : AbstractValidator<CreateBookCommand>
         RuleFor(x => x.Title).NotEmpty()
         .WithMessage("Title is required")
         .MaximumLength(200).WithMessage("Title must be less than 200 characters");
-        RuleFor(x => x.AuthorId).NotNull().NotEmpty().WithMessage("AuthorId is required");
+        RuleFor(x => x.AuthorId).NotEmpty().WithMessage("AuthorId is required");
         RuleFor(x => x.Stock).NotEmpty().WithMessage("Stock is required").GreaterThan(0).WithMessage("Stock must be greater than 0");
     }
 }
